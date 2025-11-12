9d30a17232e488671cb508d18e14641625934699
JudithUru <judith.uruchima@uees.edu.ec>
2025-11-11 19:34:45 -0500

    proyect complete

---
+++ routes/fotos.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+const Sequelize = require('sequelize');
+const Foto = require('../models').foto;
+
+router.get('/findAll/json', function (req, res, next) {
+
+    Foto.findAll({
+        attributes: {
+            exclude: ["updatedAt"]
+        }
+    }).then(fotos => {
+        res.json(fotos);
+    }).catch(error => res.status(400).send(error)
+    )
+});
+
+
+module.exports = router;
+
